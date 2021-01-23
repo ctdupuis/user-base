23929073e8537a7baf9af35f1e4357534f6f17a9
ctdupuis <ctdupuis7@gmail.com>
2021-01-23 11:52:09 -0600

    simple navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { NewUserComponent } from './new-user/new-user.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home'},
-  { path: '/home', component: HomeComponent }
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'new', component: NewUserComponent }
 ];
 
 @NgModule({
