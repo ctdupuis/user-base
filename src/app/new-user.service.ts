59908e252d2698e51fb40775efa08a11a9e10ada
ctdupuis <ctdupuis7@gmail.com>
2021-01-24 20:13:13 -0600

    form creates user and persists to database

--- src/app/new-user.service.ts
+++ src/app/new-user.service.ts
@@ -1,17 +1,24 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewUserService {
 
-  constructor() { }
+  constructor(private firestore: AngularFirestore) { }
 
   getUsers() {
     console.log("Hey I got some users here")
   }
 
   addUser(userdata: object) {
-    console.log(userdata)
+    return new Promise<any>((resolve, reject) => {
+      this.firestore
+      .collection('users')
+      .add(userdata)
+      .then(res => {}, err => reject(err))
+    })
   }
+
 }
