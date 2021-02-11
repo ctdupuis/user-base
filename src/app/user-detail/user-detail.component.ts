6b7744f16c93120f730cc4efb77e4e87ddeebe7f
ctdupuis <ctdupuis7@gmail.com>
2021-02-10 19:37:56 -0600

    Form setup in app-user-detail

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators, FormArray } from '@angular/forms';
 import { User } from '../shared/models/user.model';
-import { EmployHistory } from '../shared/models/employHistory.model';
 import { UserService } from '../user.service';
 
 @Component({
@@ -9,6 +9,29 @@ import { UserService } from '../user.service';
   styleUrls: ['./user-detail.component.scss']
 })
 export class UserDetailComponent implements OnInit {
+  editForm = new FormGroup({
+    phone: new FormControl('',
+    [Validators.required]),
+    email: new FormControl('',
+    [Validators.required]),
+    address: new FormGroup({
+      street: new FormControl('',
+      [Validators.required]),
+      city: new FormControl(''),
+      state: new FormControl(''),
+      zip: new FormControl('',
+      [Validators.required, Validators.maxLength(5)])
+    }),
+    employHistory: new FormArray([
+      new FormGroup({
+        employer: new FormControl('', [Validators.required]),
+        manager: new FormControl('', [Validators.required]),
+        startDate: new FormControl('', [Validators.required]),
+        endDate: new FormControl('', [Validators.required])
+      })
+    ])
+  })
+  employHistory = this.editForm.get('employHistory') as FormArray;
 
   @Input() user: User;
   @Input() closeDetails;
